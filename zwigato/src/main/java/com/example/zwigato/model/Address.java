96ba0c3d8a487456526cbf8557fe4bd30dee494c
ashish <ashishgaur407@gmail.com>
2025-12-17 11:14:58 +0530

    y

--- zwigato/src/main/java/com/example/zwigato/model/Address.java
+++ zwigato/src/main/java/com/example/zwigato/model/Address.java
@@ -1,5 +1,21 @@
 package com.example.zwigato.model;
 
 public class Address {
-    int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String houseNo;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+    @Column
+    private String pinCode;
+
 }
