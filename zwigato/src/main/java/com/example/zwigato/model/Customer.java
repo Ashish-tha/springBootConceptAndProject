1657daae896360b98c180348cd7348be1b50ecc0
ashish <ashishgaur407@gmail.com>
2025-12-17 11:13:25 +0530

    y

--- zwigato/src/main/java/com/example/zwigato/model/Customer.java
+++ zwigato/src/main/java/com/example/zwigato/model/Customer.java
@@ -1,4 +1,22 @@
 package com.example.zwigato.model;
 
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column(nullable = false,length = 10)
+    private String mobNo;
+
+    @Column
+    private String email;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 }
